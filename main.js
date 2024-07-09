7765b3bd127eb0b2bb53f4d69030b3c9671a436b
Saad Shafique <hafizsaad1619@gmail.com>
2024-07-09 17:37:53 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Muhammad Ali Jinnah";
+let quote = "There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women.";
+let message = `${famous_person} once said, "${quote}"`;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let famous_person : string = "Muhammad Ali Jinnah";
+let quote : string = "There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women.";
+let message : string = `${famous_person} once said, "${quote}"`
+console.log (message)
